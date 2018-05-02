56a7e6959a3d878335cc29d63b777db1ea0ffcd5
Daan Vandebosch <Daan Vandebosch@DESKTOP-AM7G2SU>
2018-05-02 10:34:18 +0200

    post

---
+++ TestProjectMobiele/TestProjectMobiele/Models/Kleuter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectMobiele.Models
+{
+    //Made by Daan Vandebosch
+    public class Kleuter
+    {
+        public string Naam;
+    }
+}
