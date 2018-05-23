172a1a7ee1e031c1bfca5bd1e0bd5f44dcf08972
Andreyski <37267550+Andreyski@users.noreply.github.com>
2018-05-23 09:31:20 +0200

    CreatePrefab

---
+++ TestProjectMobiele/TestProjectMobiele/Models/CreatePrefab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectMobiele
+{
+
+    class CreatePrefab
+    {
+
+        List<kleuter> kleuterList = new List<kleuter>();
+
+        Kleuter kleuter = new Kleuter()
+        {
+            KleuterID = 0,
+            VoorNaam = "Daan",
+            Naam = "Vandebosch",
+            SchoolID = 2,
+            FotoPad = "FotoString",
+            GezinsID = 4,
+            KlasID = 3
+
+        };
+    }
+}
